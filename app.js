654f210f7102762082523c70985bfb6199c4dad1
Necip Dirik <necipdirik@gmail.com>
2024-03-05 08:07:12 +0300

    challenge answers ordered neatly

--- app.js
+++ app.js
@@ -8,29 +8,22 @@
 //    - A variable that stores the name of an online course of your choice
 //    - A variable that stores the price of that course
 //    - A variable that stores the three main goals that you have, when taking this course
-// 2) Output ("alert") the three variable values
-// 3) Try "grouping" the three variables together and still output their values thereafter
-// 4) Also output the second element in your "main goals" variable
-// 5) Add a custom command that does the following:
-//    - Use your "main goals" variable and access an element by its identifier
-//    - The concrete identifier value should be dynamic / flexible 
-//      (i.e. the command can be executed for different identifier)
-//    - The "main goals" variable should also be dynamic: The command should work 
-//      with ANY list of values
-//    - The custom command should provide the accessed value (i.e. the list element)
-// 6) Execute your custom command from (5) and output ("alert") the result
-
-// --------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------
-// ANSWERS
 
 // let courseName = 'webDevelopment';
 // let coursePrice = 100;
 // let goals = ['coding', 'publishing', 'employment'];
 
+// --------------------------------------------------------------------------------
+
+// 2) Output ("alert") the three variable values
+
 // alert(courseName);
 // alert(coursePrice);
 // alert(goals);
+
+// --------------------------------------------------------------------------------
+
+// 3) Try "grouping" the three variables together and still output their values thereafter
 
 let course = {
   name: 'webDevelopment',
@@ -42,13 +35,29 @@ let course = {
 // alert(course.price);
 // alert(course.goals);
 
+// --------------------------------------------------------------------------------
+
+// 4) Also output the second element in your "main goals" variable
+
 // alert(course.goals[1]);
 
+// --------------------------------------------------------------------------------
 
+// 5) Add a custom command that does the following:
+//    - Use your "main goals" variable and access an element by its identifier
+//    - The concrete identifier value should be dynamic / flexible 
+//      (i.e. the command can be executed for different identifier)
+//    - The "main goals" variable should also be dynamic: The command should work 
+//      with ANY list of values
+//    - The custom command should provide the accessed value (i.e. the list element)
 
 function getListItem (array, arrayIndex) {
   arrayElement = array[arrayIndex];
   return arrayElement;
 }
+
+// --------------------------------------------------------------------------------
+
+// 6) Execute your custom command from (5) and output ("alert") the result
 
 alert(getListItem(course.goals, 0));
